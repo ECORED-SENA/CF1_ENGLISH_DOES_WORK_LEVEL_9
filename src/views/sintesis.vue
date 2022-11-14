8f68dbf4d83fa5b0384fa85fb9a63d0091632a4f
AndresDiazPinto <98177573+AndresDiazPinto@users.noreply.github.com>
2022-11-14 14:54:59 -0500

    Ajustes
    Se realizan ajustes de contenido

--- src/views/sintesis.vue
+++ src/views/sintesis.vue
@@ -9,7 +9,7 @@
           img(src="@/assets/curso/sintesis/img1.png", alt="alt")
       .col-xl-8
         p.mb-5 <em>Up to this point, we have been able to appreciate everything that has to do with Present Simple, Simple Past and Past Continuous tenses, the use of “used to” and “would” to talk about past habits, and the sequential past linkers, that is why we invite you to consider the following conceptual scheme that summarizes the subject treated:</em>
-        p.colr_2 Hasta este punto, se ha podido apreciar todo lo que tiene que ver con el tiempo presente simple, el pasado simple y el pasado continuo, el uso de "used to" y "would" para hablar de hábitos pasados, y los conectores de pasado secuencial, por lo que le invitamos a considerar el siguiente esquema conceptual que resume el tema tratado: 
+        p.colr_2 Hasta este punto, se ha podido apreciar todo lo que tiene que ver con el tiempo presente simple, el pasado simple y el pasado continuo, el uso de "<em>used to</em>" y "<em>would</em>" para hablar de hábitos pasados, y los conectores de pasado secuencial, por lo que le invitamos a considerar el siguiente esquema conceptual que resume el tema tratado: 
 
     .row.justify-content-center
       .col-lg-10.mb-5
